bc1e14a0ea325ddb60b09b380fb9e8fa2a10a6d0
Antoine07 <antoine.lucsko@gmail.com>
2021-04-06 15:22:05 +0200

    bar

---
+++ Symfony/bar/src/Controller/BarController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BarController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('bar/index.html.twig');
+    }
+}
