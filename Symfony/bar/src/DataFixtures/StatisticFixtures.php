262b7e2c45a5a4897b9299f4386d423181f6264d
Antoine07 <antoine.lucsko@gmail.com>
2021-04-12 09:42:55 +0200

    statistic fixtures

---
+++ Symfony/bar/src/DataFixtures/StatisticFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class StatisticFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            AppFixtures::class,
+        ];
+    }
+}
