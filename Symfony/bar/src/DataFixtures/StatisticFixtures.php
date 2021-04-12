62f7e298d07a7dbcf7a5d220680c46f942e9f465
Antoine07 <antoine.lucsko@gmail.com>
2021-04-12 11:21:13 +0200

    bar

--- Symfony/bar/src/DataFixtures/StatisticFixtures.php
+++ Symfony/bar/src/DataFixtures/StatisticFixtures.php
@@ -2,17 +2,60 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+use Faker;
+use App\Entity\Client;
+use App\Entity\Statistic;
+use App\Entity\Beer;
 
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class StatisticFixtures extends Fixture implements DependentFixtureInterface
 {
+    const NB_CLIENT = 10;
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
+
+        $count = 0;
+        while ($count < self::NB_CLIENT) {
+            $client = new Client();
+            $client->setName($faker->firstName);
+            $client->setEmail($faker->email);
+            $client->setAge(random_int(18, 70));
+            $manager->persist($client);
+
+            $count++;
+        }
+
+        $manager->flush();
+
+        $clients = $manager->getRepository(Client::class)->findAll();
+        $beers = $manager->getRepository(Beer::class)->findAll();
+
+        $count = 0;
+        while ($count < self::NB_CLIENT) {
+            $countBeer = 0;
+            while ($countBeer < random_int(2, 7)) {
+                $stat = new Statistic();
+                $stat->setClient($clients[$count]);
+                $stat->setBeer($beers[$countBeer]);
+                $stat->setScore(rand(0, 5));
+                $stat->setNumberBeer(rand(1, 10));
+
+                $date = new \DateTime('2000-01-01');
+                $day = random_int(10, 2500);
+                $date->add(new \DateInterval("P" . $day . "D"));
+                $stat->setCreatedAt($date);
+
+                $manager->persist($stat);
+                $countBeer++;
+            }
+
+            $count++;
+        }
 
         $manager->flush();
     }
