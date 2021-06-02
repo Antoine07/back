6481482d60a9ee961e6b7db3f1b393c0e68dc7ad
Antoine07 <antoine.lucsko@gmail.com>
2021-06-02 12:07:04 +0200

    resto

--- NodeJS/Introduction/Exercices/app_restaurants/server.js
+++ NodeJS/Introduction/Exercices/app_restaurants/server.js
@@ -8,13 +8,12 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 const dbName = "ny";
 const collectionName = "restaurants";
+let [restaurants, cuisines] = [null, null];
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-let [restaurants, cuisines] = [null, null];
 
 const run = async () => {
   try {
@@ -38,84 +37,113 @@ const pagesPath = path.join(__dirname, "/pages"); // les pages que je vais envoy
 const viewsPath = path.join(__dirname, "/views"); // le templating
 const dataPath = path.join(__dirname, "/data"); // les données
 
+const limit_doc = 10;
+
+// fonctions utiles
+const handleError = (req, res) => {
+  res.writeHead(404, { "Content-type": "text/html" });
+  res.write("<html><head></head><body><h1>Error</h1></body></html>");
+  res.end();
+};
+
+//
+const formatDigit = (num, prec = 1_00_000) => Math.floor(num * prec) / prec;
+
 const server = http.createServer((req, res) => {
+  // traiter assets fichiers statiques
   if (req.url.match(/\.css$/)) {
-    const cssPath = `${pagesPath}/css/styles.css`;
-    const css = fs.readFileSync(cssPath, "utf8");
-    res.writeHead(200, { "Content-Type": "text/css" });
-    res.write(css);
+    const cssPath =
+      (req.url.match(/bootstrap/) ? __dirname : `${pagesPath}`) + req.url;
+    const fileCss = fs.readFileSync(cssPath, "utf8");
+
+    res.writeHead(200, { "Content-type": "text/css" });
+    res.write(fileCss);
     res.end();
   } else {
-    // GET
     if (req.method === "GET") {
-      restaurants.distinct("cuisine").then((cuisines) => {
-        ejs.renderFile(`${viewsPath}/index.ejs`, { cuisines }, (err, str) => {
-          res.writeHead(200, { "Content-type": "text/html" });
-          res.write(str);
-          res.end();
-        });
-      });
+      const meta = { cuisines: [], borough: [] };
+
+      restaurants
+        .distinct("cuisine")
+        .then((cuisines) => (meta["cuisines"] = cuisines))
+        .then(() => restaurants.distinct("borough"))
+        .then((borough) => (meta["borough"] = borough))
+        .then(() => {
+          const { cuisines, borough } = meta;
+          ejs.renderFile(
+            `${viewsPath}/index.ejs`,
+            { cuisines, borough, title: "Bonjour EJS MongoDB & Node.js" },
+            (err, str) => {
+              if (err) {
+                handleError(req, res);
+
+                return;
+              }
+              res.writeHead(200, { "Content-type": "text/html" });
+              res.write(str);
+              res.end();
+            }
+          );
+        })
+        .catch(() => handleError(req, res));
     }
 
-    // POST
     if (req.method === "POST") {
       let body = "";
-      req.on("data", (data) => {
-        body += data;
+
+      // par morceau
+      req.on("data", (chunk) => {
+        body += chunk;
       });
 
       req.on("end", async () => {
-        console.log("Body: " + body);
-        const lenSanitize = body.length - "=&Search=Envoyer".length;
-
-        const data = body
-          .slice(0, lenSanitize + 1)
+        const [borough, cuisine] = body
           .split("&")
           .map((str) => str.split("=")[1]);
-
-        const [borough, cuisine] = data;
 
         let results = await restaurants
           .aggregate([
             {
               $match: {
-                borough: new RegExp(borough, "i"),
+                borough: new RegExp(borough, "i"), // ils sont déjà dans un select donc ce n'est pas nécessaire de passer une regex
                 cuisine: new RegExp(cuisine, "i"),
               },
             },
             {
               $project: {
-                name: 1,
                 _id: 0,
+                name: 1,
                 "address.coord": 1,
               },
             },
-            { $limit: 10 },
+            { $limit: limit_doc },
           ])
           .toArray();
 
-        // mapping pour préparer les données
-        const formatDigit = (num) =>
-          num ? Math.floor(num * 1000) / 1000 : null;
-
-        results = results.map((res) => ({
-          lat: formatDigit(res?.address.coord[0]),
-          lng: formatDigit(res?.address.coord[1]),
-          name: res.name,
+        // attention aux parenthèses elle force le retour du littéral donc ici il n'y pas de problème de syntaxe.
+        results = results.map((result) => ({
+          lat: formatDigit(result.address.coord[1]),
+          lng: formatDigit(result.address.coord[0]),
+          name: result.name,
         }));
+
+        console.log(results);
 
         ejs.renderFile(
           `${viewsPath}/results.ejs`,
-          {
-            results,
-            borough,
-          },
+          { results, borough },
           (err, str) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
+            if (err) {
+              handleError(req, res);
+
+              return;
+            }
+            res.writeHead(200, { "Content-type": "text/html" });
             res.write(str);
             res.end();
           }
         );
+
       });
     }
   }
